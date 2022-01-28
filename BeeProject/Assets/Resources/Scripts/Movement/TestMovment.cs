50e88014eff43e885e77eaded55c52e1e202b69e
clmeyrs <89176352+clmeyrs@users.noreply.github.com>
2022-01-28 02:02:23 -0500

    Added bullets and improved the movement

--- BeeProject/Assets/Resources/Scripts/Movement/TestMovment.cs
+++ BeeProject/Assets/Resources/Scripts/Movement/TestMovment.cs
@@ -11,20 +11,35 @@ public class TestMovment : MonoBehaviour
     private float downwardSpeed = 6;
      [SerializeField]
     private float LeftAndRight = 6;
+    [SerializeField]
+    private float moveSpeedIncrement =.1f;
+    [SerializeField]
+    private float SpeedCap = 20f;
+
     void Start()
     {
         
     }
-
+    void  Update()
+    {
+    if(downwardSpeed <SpeedCap)
+        downwardSpeed+=moveSpeedIncrement *Time.deltaTime;
+    }
+    public void slow(){
+        downwardSpeed = downwardSpeed/2;
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.Translate(Vector3.down*downwardSpeed*Time.deltaTime);
-        if(Input.GetKey(KeyCode.UpArrow)){
-           transform.Translate(Vector3.up*upAndDown*Time.deltaTime);
+        if(Input.GetKey(KeyCode.UpArrow)&&downwardSpeed>6){
+            downwardSpeed -=(upAndDown*2)*Time.deltaTime;
+         //  transform.Translate(Vector3.up*upAndDown*Time.deltaTime);
        }
-        if(Input.GetKey(KeyCode.DownArrow)){
-            transform.Translate(Vector3.down*upAndDown*Time.deltaTime);
+        if(Input.GetKey(KeyCode.DownArrow)&&downwardSpeed<SpeedCap){
+
+             downwardSpeed +=upAndDown*Time.deltaTime;
+           // transform.Translate(Vector3.down*upAndDown*Time.deltaTime);
         }
         if(Input.GetKey(KeyCode.LeftArrow)){
             transform.Translate(Vector3.left*LeftAndRight*Time.deltaTime);
--- BeeProject/Assets/Resources/Scripts/Projectile/Fire.cs
+++ BeeProject/Assets/Resources/Scripts/Projectile/Fire.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
+
+     [SerializeField]
+    private float fireangle;
+    [SerializeField]
+    private float fireSpeed = 0.5f;
+    [SerializeField]
+    private float timer;
     public Transform firepoint;
     [SerializeField]
     private GameObject bullet;
 
     public void  Update()
     {
+        if(timer <=0){
         if(Input.GetKey(KeyCode.Space)){
-            Instantiate(bullet,firepoint.position,Quaternion.identity);
+
+            Instantiate(bullet,firepoint.position,Quaternion.Euler(new Vector3(0,0,fireangle)));
+            timer = fireSpeed;
+        }
+        }else{
+            timer -= Time.deltaTime;
         }
     }
 }
