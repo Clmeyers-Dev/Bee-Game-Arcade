6e9ce7264a6ee4b39daf6c01b2ec54c43e78f3f9
Cassidy Meyers <70337853+Clmeyers-Dev@users.noreply.github.com>
2024-01-10 09:46:25 -0500

    Update LaserTut.cs

--- BeeProject/Assets/Resources/Scripts/Projectile/LaserTut.cs
+++ BeeProject/Assets/Resources/Scripts/Projectile/LaserTut.cs
@@ -9,88 +9,89 @@ public class LaserTut : MonoBehaviour
     public Transform firePoint;
     public GameObject startVFX;
     public GameObject endVFX;
-    [SerializeField]
-    public AudioSource audioSource;
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip sound;
+
     private Quaternion rotation;
-    [SerializeField]
-    private AudioClip sound;
     private List<ParticleSystem> particles = new List<ParticleSystem>();
+
+    public float laserDamage;
+    public LayerMask mask;
 
     void Start()
     {
         FillLists();
         DisableLaser();
         cam = Camera.main;
-       
+        audioSource = GetComponent<AudioSource>();
     }
-    
-void Awake()
-{
-    FillLists();
-    DisableLaser();
-}
-public float laserDamage;
+
+    void Awake()
+    {
+        FillLists();
+        DisableLaser();
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
-   
-        
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            
             EnableLaser();
         }
-        
-    
-        if(Input.GetKey(KeyCode.Space))
+
+        if (Input.GetKey(KeyCode.Space))
         {
-            if(lineRenderer.enabled == false){
+            if (!lineRenderer.enabled)
+            {
                 EnableLaser();
             }
-        
+
             UpdateLaser();
         }
 
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             DisableLaser();
-             audioSource.Stop();
-        }else{
-           
+            audioSource.Stop();
         }
-
-      //  RotateToMouse();
     }
 
     void EnableLaser()
     {
         lineRenderer.enabled = true;
 
-        for(int i=0; i<particles.Count; i++)
-            particles[i].Play();
+        foreach (ParticleSystem ps in particles)
+        {
+            ps.Play();
+        }
     }
-    public LayerMask mask;
+
     void UpdateLaser()
     {
-          var mousePos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
-        lineRenderer.SetPosition(0, (Vector2)firePoint.position);
-        startVFX.transform.position = (Vector2)firePoint.position;
-        if(mousePos.y <transform.position.y)
-        lineRenderer.SetPosition(1, mousePos);
+        lineRenderer.SetPosition(0, firePoint.position);
+        startVFX.transform.position = firePoint.position;
+
+        if (mousePos.y < transform.position.y)
+            lineRenderer.SetPosition(1, mousePos);
 
         Vector2 direction = mousePos - (Vector2)transform.position;
-        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, direction.normalized, direction.magnitude,mask);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, direction.normalized, direction.magnitude, mask);
 
-        if(hit)
+        if (hit)
         {
             lineRenderer.SetPosition(1, hit.point);
-            if(hit.collider.gameObject.GetComponent<EnemyManager>()!=null){
+
+            if (hit.collider.gameObject.GetComponent<EnemyManager>() != null)
+            {
                 hit.collider.gameObject.GetComponent<EnemyManager>().hurt(laserDamage);
-                if(!audioSource.isPlaying)
-                audioSource.PlayOneShot(sound);
+
+                if (!audioSource.isPlaying)
+                    audioSource.PlayOneShot(sound);
             }
-        }else{
-           
         }
 
         endVFX.transform.position = lineRenderer.GetPosition(1);
@@ -100,31 +101,25 @@ public float laserDamage;
     {
         lineRenderer.enabled = false;
 
-        for(int i=0; i<particles.Count; i++)
-            particles[i].Stop();
-    }
-
-    void RotateToMouse ()
-    {
-        Vector2 direction = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rotation.eulerAngles = new Vector3(0,0,angle);
-        transform.rotation = rotation;
+        foreach (ParticleSystem ps in particles)
+        {
+            ps.Stop();
+        }
     }
 
     void FillLists()
     {
-        for(int i=0; i<startVFX.transform.childCount; i++)
+        foreach (Transform child in startVFX.transform)
         {
-            var ps = startVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
-            if(ps != null)
+            var ps = child.GetComponent<ParticleSystem>();
+            if (ps != null)
                 particles.Add(ps);
         }
 
-        for(int i=0; i<endVFX.transform.childCount; i++)
+        foreach (Transform child in endVFX.transform)
         {
-            var ps = endVFX.transform.GetChild(i).GetComponent<ParticleSystem>();
-            if(ps != null)
+            var ps = child.GetComponent<ParticleSystem>();
+            if (ps != null)
                 particles.Add(ps);
         }
     }
