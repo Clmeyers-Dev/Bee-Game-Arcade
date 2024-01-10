33e2d16919d6cb923cf8b97cf5c9ba83bdbe7123
Cassidy Meyers <70337853+Clmeyers-Dev@users.noreply.github.com>
2024-01-10 09:51:52 -0500

    Update DragonFly.cs

--- BeeProject/Assets/Resources/Scripts/Movement/DragonFly.cs
+++ BeeProject/Assets/Resources/Scripts/Movement/DragonFly.cs
@@ -1,48 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DragonFly : MonoBehaviour
 {
-   // Start is called before the first frame update
     private Vector3 start;
- //[SerializeField]
-    //private float freq = 5f;
-    //[SerializeField]
-   // private float mag = 5f;
-    //[SerializeField]
-  //  private float offset = 0f;
-    [SerializeField]
-    float speed = 5f;
-      [SerializeField]
-    float Vertspeed = 5f;
+    [SerializeField] private float horizontalSpeed = 5f;
+    [SerializeField] private float verticalSpeed = 5f;
+    [SerializeField] private float maxTime = 5f; // Adjust the default value as needed
 
-    [SerializeField]
-    float maxTime;
-    [SerializeField]
-    float time;
-    public bool movingRight;
+    private float timer;
+    private bool movingRight;
+
     void Start()
     {
         start = transform.position;
-        maxTime = Random.Range(1,6);
+        ResetTimer();
     }
 
-    // Update is called once per frame
     void Update()
     {
-      transform.Translate(Vector3.down*Vertspeed*Time.deltaTime);
-      // transform.position =  start +new Vector3(Mathf.Sin(Time.time),0.0f, 0.0f);
-       if(time >= maxTime){
-           movingRight = !movingRight;
-           time = 0;
-       }else{
-           time += Time.deltaTime;
-       }
-     if(movingRight){
-         transform.Translate(-Vector3.left * speed * Time.deltaTime);
-     }else{
-         transform.Translate(Vector3.left*speed*Time.deltaTime);
-     }
+        MoveUpDown();
+        MoveHorizontally();
+    }
+
+    void MoveUpDown()
+    {
+        transform.Translate(Vector3.down * verticalSpeed * Time.deltaTime);
+    }
+
+    void MoveHorizontally()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= maxTime)
+        {
+            movingRight = !movingRight;
+            ResetTimer();
+        }
+
+        if (movingRight)
+        {
+            transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+        }
+    }
+
+    void ResetTimer()
+    {
+        timer = 0f;
+        maxTime = Random.Range(1f, 6f); // Adjust the range of random values as needed
     }
 }
