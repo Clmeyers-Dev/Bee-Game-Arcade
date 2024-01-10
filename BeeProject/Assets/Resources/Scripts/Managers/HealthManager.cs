5920da10a9e305742d13918b985da92d2931f710
Cassidy Meyers <70337853+Clmeyers-Dev@users.noreply.github.com>
2024-01-10 09:25:20 -0500

    Update HealthManager.cs

--- BeeProject/Assets/Resources/Scripts/Managers/HealthManager.cs
+++ BeeProject/Assets/Resources/Scripts/Managers/HealthManager.cs
@@ -4,42 +4,43 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField]
-    private GameObject[] healthBars = new GameObject [3];
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] healthBars = new GameObject[3];
 
-    // Update is called once per frame
-    void Update()
+    public void LoseHealth()
     {
-        
-    }
-    public void loseHealth(){
-        for(int i = 0; i < healthBars.Length;i++){
-            if(healthBars[i].activeSelf){
+        for (int i = 0; i < healthBars.Length; i++)
+        {
+            if (healthBars[i].activeSelf)
+            {
                 healthBars[i].SetActive(false);
                 return;
             }
         }
     }
-    public void gainHealth(){
-        for(int i = healthBars.Length-1;i>=0;i--){
-            if(!healthBars[i].activeSelf){
+
+    public void GainHealth()
+    {
+        for (int i = healthBars.Length - 1; i >= 0; i--)
+        {
+            if (!healthBars[i].activeSelf)
+            {
                 healthBars[i].SetActive(true);
                 return;
             }
         }
     }
-    public int getNumberOfHealth(){
-        int count=0;
-       for(int i = 0; i < healthBars.Length;i++){
-           if(healthBars[i].activeSelf){
-               count++;
-           }
-       }
+
+    public int GetNumberOfHealth()
+    {
+        int count = 0;
+
+        for (int i = 0; i < healthBars.Length; i++)
+        {
+            if (healthBars[i].activeSelf)
+            {
+                count++;
+            }
+        }
 
         return count;
     }
