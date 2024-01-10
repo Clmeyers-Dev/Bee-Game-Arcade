d2ec421462c326a22e6d77e3d5477c9663a0fad6
Cassidy Meyers <70337853+Clmeyers-Dev@users.noreply.github.com>
2024-01-10 09:50:06 -0500

    Update Beetle.cs

--- BeeProject/Assets/Resources/Scripts/Movement/Beetle.cs
+++ BeeProject/Assets/Resources/Scripts/Movement/Beetle.cs
@@ -1,32 +1,38 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Beetle : MonoBehaviour
 {
     [SerializeField]
-     private float RotateSpeed = 5f;
-     [SerializeField]
-     private float Radius = 0.1f;
- 
-    
-     [SerializeField]
-     private float _angle;
-     [SerializeField]
-     private GameObject _center;
- 
-      void Start()
-     {
-         
-     }
- 
-      void Update()
-     {
- 
-         _angle += RotateSpeed * Time.deltaTime;
- 
-         var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
-         transform.position = (Vector2)_center.transform.position + offset;
-     }
-  
+    private float moveSpeed = 2f;
+    [SerializeField]
+    private float zigzagAmplitude = 2f;
+    [SerializeField]
+    private float zigzagFrequency = 2f;
+
+    private Vector2 startPosition;
+    private float timeElapsed;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        MoveInZigzagPattern();
+    }
+
+    void MoveInZigzagPattern()
+    {
+        timeElapsed += Time.deltaTime;
+
+        float xOffset = Mathf.Sin(timeElapsed * zigzagFrequency) * zigzagAmplitude;
+        float yOffset = Mathf.Cos(timeElapsed * zigzagFrequency) * zigzagAmplitude;
+
+        Vector2 zigzagOffset = new Vector2(xOffset, yOffset);
+        Vector2 newPosition = startPosition + zigzagOffset;
+
+        transform.position = Vector2.MoveTowards(transform.position, newPosition, moveSpeed * Time.deltaTime);
+    }
 }
