f39537baaf97100d2c51201778ee2777509d1ed5
Cassidy Meyers <70337853+Clmeyers-Dev@users.noreply.github.com>
2024-01-10 09:43:49 -0500

    Update Fire.cs

--- BeeProject/Assets/Resources/Scripts/Projectile/Fire.cs
+++ BeeProject/Assets/Resources/Scripts/Projectile/Fire.cs
@@ -1,51 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-
-     [SerializeField]
-    private float fireangle;
     [SerializeField]
-    public float fireSpeed = 0.5f;
+    private float fireangle;
+    
+    [SerializeField]
+    private float fireSpeed = 0.5f;
+    
     [SerializeField]
     private float timer;
+
     public Transform firepoint;
+    
     [SerializeField]
     private GameObject bullet;
-    public GameObject parent;
+
     public PlayerManager player;
-    [SerializeField]
-    public float burstFireSpeed =1;
     
-   public AudioSource audioPlayer;
-  public  AudioClip sound;
-  public float currentSpeed;
-  public void setBullet(GameObject b){
- bullet = b;
-  }
-  public void setFireSpeed(float s){
-    currentSpeed = s;
-  }
+    public float burstFireSpeed = 1;
+
     void Start()
     {
-      currentSpeed = fireSpeed;
-       // parent = GetComponentInParent<GameObject>();
-       player = GetComponentInParent<PlayerManager>();
-      // audioPlayer = FindObjectOfType<AudioSource>();
+        player = GetComponentInParent<PlayerManager>();
     }
-    public void  Update()
-    {
-        if(timer <=0){
-        if(Input.GetKey(KeyCode.Space)){
 
-  var shot =   Instantiate(bullet,firepoint.position,Quaternion.Euler(parent.transform.localEulerAngles.x,parent.transform.localEulerAngles.y,parent.transform.localEulerAngles.z+fireangle) );
-    audioPlayer.PlayOneShot(sound);
-            //shot.GetComponent<Projectile>().setSpeed( player.getSpeed() + 20);
-            timer = currentSpeed;
+    void Update()
+    {
+        if (timer <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                var shot = Instantiate(bullet, firepoint.position, Quaternion.Euler(parent.transform.localEulerAngles.x, parent.transform.localEulerAngles.y, parent.transform.localEulerAngles.z + fireangle));
+                GetComponent<AudioSource>().PlayOneShot(sound);
+                timer = currentSpeed;
+            }
         }
-        }else{
+        else
+        {
             timer -= Time.deltaTime;
         }
     }
