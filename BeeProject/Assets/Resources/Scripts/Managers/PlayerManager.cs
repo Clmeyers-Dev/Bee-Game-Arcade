f05e8cb871e9aaac2432475d1bf96588fb922670
Cassidy Meyers <70337853+Clmeyers-Dev@users.noreply.github.com>
2024-01-10 09:33:43 -0500

    Update PlayerManager.cs

--- BeeProject/Assets/Resources/Scripts/Managers/PlayerManager.cs
+++ BeeProject/Assets/Resources/Scripts/Managers/PlayerManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Cinemachine;
@@ -7,182 +6,157 @@ using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //[SerializeField]
-    //private float upAndDown = 6;
-    private HealthManager healthManager;
-    [SerializeField]
-    private float downwardSpeed = 6;
-    [SerializeField]
-    private float LeftAndRight = 6;
-   // [SerializeField]
-   // private float moveSpeedIncrement = .1f;
-   // [SerializeField]
-   // private float SpeedCap = 20f;
-    public GameObject left;
-    public GameObject right;
-     public GameObject top;
-    public GameObject bottom;
-    public float score;
-     public CinemachineImpulseSource	impulse;
-    public TextMeshProUGUI scoreText;
-    public AudioSource audioSource;
-    public float flashLength = 0f;
+    [Header("Movement Settings")]
+    [SerializeField] private float downwardSpeed = 6;
+    [SerializeField] private float leftAndRightSpeed = 6;
+    [SerializeField] private float rightOffset = 0;
+    [SerializeField] private float leftOffset = 0;
+    [SerializeField] private float topOffset = 0;
+    [SerializeField] private float bottomOffset = 0;
+
+    [Header("References")]
+    [SerializeField] private HealthManager healthManager;
+    [SerializeField] private GameObject leftBoundary;
+    [SerializeField] private GameObject rightBoundary;
+    [SerializeField] private GameObject topBoundary;
+    [SerializeField] private GameObject bottomBoundary;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private CinemachineImpulseSource impulse;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private SpriteRenderer playerSprite;
+    [SerializeField] private SpriteRenderer[] spriteArray = new SpriteRenderer[1];
+
+    [Header("Flash Effect Settings")]
+    [SerializeField] private float flashLength = 0f;
     private float flashCounter = 0f;
-    public SpriteRenderer playerSprite;
-	public bool flashActive;
-    public SpriteRenderer [] spriteArray  = new SpriteRenderer [1]; 
+    private bool flashActive;
+
+    [Header("Hit Settings")]
+    [SerializeField] private float hitTime = 0;
+    private float curHitTime;
+
+    [Header("Score Settings")]
+    public float score;
+    [SerializeField] private float scoreModifier = 1;
+
+    [Header("Scene Settings")]
+    [SerializeField] private SceneEnum sceneToLoad;
+
+    public Fire playerMainGunFire;
+
     void Start()
     {
         healthManager = FindObjectOfType<HealthManager>();
     }
+
     void Update()
     {
-     // Debug.Log( healthManager.getNumberOfHealth());
-     if(healthManager.getNumberOfHealth()<=0){
-         Die();
-     }
-        scoreText .text = score+"";
-       // if (downwardSpeed < SpeedCap)
-         //   downwardSpeed += moveSpeedIncrement * Time.deltaTime;
-           if (flashActive)
+        HandleFlashEffect();
+        UpdateScoreText();
+
+        if (healthManager.getNumberOfHealth() <= 0)
         {
-            if (flashCounter > flashLength * .99f)
-            {
-                foreach(SpriteRenderer sprite in spriteArray){
-                    if(sprite!=null)
-                    sprite.color =new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
-                }
-              //  playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
-            }
-            else if (flashCounter > flashLength * .82f)
-            {
-                foreach(SpriteRenderer sprite in spriteArray){
-                    if(sprite!=null)
-                    sprite.color =new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
-                }
-               // playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
-            }
-            else if (flashCounter > flashLength * .66f)
-            {
-                foreach(SpriteRenderer sprite in spriteArray){
-                    if(sprite!=null)
-                    sprite.color =new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
-                }
-               // playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
-            }
-            else if (flashCounter > flashLength * .49f)
-            {
-                foreach(SpriteRenderer sprite in spriteArray){
-                    if(sprite!=null)
-                    sprite.color =new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
-                }
-              //  playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
-            }
-            else if (flashCounter > flashLength * .33f)
-            {
-                foreach(SpriteRenderer sprite in spriteArray){
-                    if(sprite!=null)
-                    sprite.color =new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
-                }
-              //  playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
-            }
-            else if (flashCounter > flashLength * .16f)
-            {
-                foreach(SpriteRenderer sprite in spriteArray){
-                    if(sprite!=null)
-                    sprite.color =new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
-                }
-               // playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
-            }
-            else if (flashCounter > 0f)
-            {
-                foreach(SpriteRenderer sprite in spriteArray){
-                    if(sprite!=null)
-                    sprite.color =new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
-                }
-               // playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0f);
-            }
-            else
-            {
-                foreach(SpriteRenderer sprite in spriteArray){
-                    if(sprite!=null)
-                    sprite.color =new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
-                }
-                //playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
-                flashActive = false;
-            }
-            flashCounter -= Time.deltaTime;
+            Die();
         }
     }
-    public float getSpeed()
-    {
-        return downwardSpeed;
-    }
-    public void slow()
-    {
-        downwardSpeed = downwardSpeed / 2;
-    }
-    [SerializeField]
-   private SceneEnum sceneToLoad;
-    private void Die(){
-             SceneManager.LoadSceneAsync(sceneToLoad.ToString());
-    }
-    public Fire playerMainGunFire;
-    // Update is called once per frame
-    [SerializeField]
-    private float Rightoffset;
-    [SerializeField]
-    private float LeftOffset;
-      [SerializeField]
-    private float topOffset;
-    [SerializeField]
-    private float BottomOffset;
-    public float modifier = 1;
-    public void addPoints(float p){
-        impulse.GenerateImpulse();
-        score += (p*modifier);
-    }
+
     void FixedUpdate()
     {
-        //   transform.Translate(Vector3.down*downwardSpeed*Time.deltaTime);
-        
-        if (Input.GetKey(KeyCode.W) && transform.position.y < top.transform.position.y+topOffset)// downwardSpeed > 6)
-        {
-            // downwardSpeed -=(upAndDown*2)*Time.deltaTime;
-            transform.Translate(Vector3.up * LeftAndRight * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S) && transform.position.y > bottom.transform.position.y+BottomOffset )//downwardSpeed < SpeedCap)
-        {
-
-            //downwardSpeed +=upAndDown*Time.deltaTime;
-            transform.Translate(Vector3.down * LeftAndRight * Time.deltaTime);
-        }
-        
-        if (Input.GetKey(KeyCode.A)&&transform.position.x>left.transform.position.x+LeftOffset)
-        {
-            transform.Translate(Vector3.left * LeftAndRight * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D)&&transform.position.x<right.transform.position.x+Rightoffset)
-        {
-            transform.Translate(-Vector3.left * LeftAndRight * Time.deltaTime);
-        }
-        if(curHitTime<=hitTime){
-            curHitTime+=Time.deltaTime;
-        }
+        HandleMovementInput();
+        curHitTime += Time.deltaTime;
     }
-    [SerializeField]
-    private float hitTime;
-    private float curHitTime;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-    //    Debug.Log("hit");
-        if(other.tag == "collision"&&curHitTime>=hitTime){
-            //TODO  create an explosion on impact 
-              flashActive = true;
-         flashCounter = flashLength;
-            curHitTime = 0;
-            healthManager.loseHealth();
+        if (other.tag == "collision" && curHitTime >= hitTime)
+        {
+            HandlePlayerCollision();
         }
+    }
+
+ void HandleFlashEffect()
+{
+    if (flashActive)
+    {
+        // Determine the alpha value based on the flashCounter
+        float alpha = Mathf.Lerp(0f, 1f, flashCounter / flashLength);
+
+        // Apply the alpha value to all sprite renderers in spriteArray
+        foreach (SpriteRenderer sprite in spriteArray)
+        {
+            if (sprite != null)
+            {
+                sprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, alpha);
+            }
+        }
+
+        // Decrease the flashCounter
+        flashCounter -= Time.deltaTime;
+
+        // Check if the flashing duration has ended
+        if (flashCounter <= 0f)
+        {
+            // Reset the sprite colors to fully visible
+            foreach (SpriteRenderer sprite in spriteArray)
+            {
+                if (sprite != null)
+                {
+                    sprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+                }
+            }
+
+            // Deactivate the flash effect
+            flashActive = false;
+        }
+    }
+}
+
+
+    void HandleMovementInput()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horizontalInput * leftAndRightSpeed * Time.deltaTime,
+                                       verticalInput * downwardSpeed * Time.deltaTime, 0);
+
+        transform.Translate(movement);
+
+        ClampPositionToBounds();
+    }
+
+    void ClampPositionToBounds()
+    {
+        float clampedX = Mathf.Clamp(transform.position.x, leftBoundary.transform.position.x + leftOffset,
+                                                    rightBoundary.transform.position.x - rightOffset);
+
+        float clampedY = Mathf.Clamp(transform.position.y, bottomBoundary.transform.position.y + bottomOffset,
+                                                    topBoundary.transform.position.y - topOffset);
+
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = score + "";
+    }
+
+    void HandlePlayerCollision()
+    {
+        flashActive = true;
+        flashCounter = flashLength;
+        curHitTime = 0;
+        healthManager.loseHealth();
+    }
+
+    void Die()
+    {
+        SceneManager.LoadSceneAsync(sceneToLoad.ToString());
+    }
+
+    public void AddPoints(float points)
+    {
+        impulse.GenerateImpulse();
+        score += points * scoreModifier;
     }
 }
